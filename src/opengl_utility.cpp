a09ac9db6a32fc367b3f75a666dce46cd476238e
joevenzon <joevenzon@f68921f7-4efc-0310-90ae-aa24c258f575>
2009-09-12 14:38:31 +0000

    Disable OpenGL error checking on Windows due to intel chipset driver bugs.

--- src/opengl_utility.cpp
+++ src/opengl_utility.cpp
@@ -2,6 +2,10 @@
 
 void OPENGL_UTILITY::CheckForOpenGLErrors(std::string activity_description, std::ostream & error_output)
 {
+#if defined(WIN32) || defined(_WIN32) || defined (__WIN32) || defined(__WIN32__) \
+	|| defined (_WIN64) || defined(__CYGWIN__) || defined(__MINGW32__)
+	//disable error checking because intel has many driver chipsets out with non-compliant OpenGL drivers
+#else
 	GLenum gl_error = glGetError();
 	if (gl_error != GL_NO_ERROR)
 	{
@@ -9,4 +13,5 @@ void OPENGL_UTILITY::CheckForOpenGLErrors(std::string activity_description, std:
 		error_output << "OpenGL error \"" << err_string << "\" during: " << activity_description << std::endl;
 		assert (gl_error == GL_NO_ERROR);
 	}
+#endif
 }
