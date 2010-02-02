9b1e6e6c4b9504cc714a1aab2e2c00e64d9ed10e
NaN <NaN@f68921f7-4efc-0310-90ae-aa24c258f575>
2010-02-02 08:25:40 +0000

    using negative vertical car velocity to offset wheel ray start position

--- src/game.cpp
+++ src/game.cpp
@@ -1631,7 +1631,7 @@ void GAME::UpdateCarWheelCollisions(CAR & car, std::vector <COLLISION_CONTACT> &
 		MATHVECTOR <float, 3> dir = car.GetDownVector();
 		MATHVECTOR <float, 3> raystart = wp;
 		float raylen = 10.0;
-		float moveback = car.GetVelocity()[2]; // don't want to fall through the ground
+		float moveback = -car.GetVelocity()[2]; // don't want to fall through the ground
 		if (moveback > 1) cout << "moveback = " << moveback << endl;
 		if (moveback < 0)
 			moveback = 0;
