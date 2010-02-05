3931d3a4c77f65fa324577e9ec5c55bbee9d65d5
NaN <NaN@f68921f7-4efc-0310-90ae-aa24c258f575>
2010-02-05 19:27:37 +0000

    create definitions.h if not existing

--- tools/win/bin/build_vdrift.bat
+++ tools/win/bin/build_vdrift.bat
@@ -6,6 +6,19 @@ echo Make sure you're running this from your VDrift folder, like so:
 echo tools\win\bin\build_vdrift.bat
 
 echo.
+set def=include\definitions.h
+if exist %def% goto DONE
+echo %def% not found. Generating %def%
+echo #ifndef _DEFINITIONS_H>%def%
+echo #define _DEFINITIONS_H>>%def%
+echo #define SETTINGS_DIR ".vdrift">>%def%
+echo #define DATA_DIR "./data">>%def%
+echo #define VERSION "%date:~-4%-%date:~3,2%-%date:~0,2%">>%def%
+echo #define REVISION "latest">>%def%
+echo #endif>>%def%
+:DONE
+
+echo.
 call scons release=1
 
 echo.
