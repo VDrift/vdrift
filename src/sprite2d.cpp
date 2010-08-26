1703e9f74aec3522b4d5bf38f006ac0d35ad0617
NaN <NaN@f68921f7-4efc-0310-90ae-aa24c258f575>
2010-08-26 17:45:51 +0000

    sprite2d.cpp

---
+++ src/sprite2d.cpp
@@ -0,0 +1,78 @@
+#include "sprite2d.h"
+
+void SPRITE2D::Unload(SCENENODE & parent)
+{
+	if (node.valid())
+	{
+		SCENENODE & noderef = GetNode(parent);
+		noderef.GetDrawlist().twodim.erase(draw);
+		parent.Delete(node);
+	}
+	node.invalidate();
+	draw.invalidate();
+	
+	varray.Clear();
+}
+
+bool SPRITE2D::Load(
+	SCENENODE & parent,
+	const std::string & texturefile,
+	const std::string & texturesize,
+	MANAGER<TEXTURE, TEXTUREINFO> & textures,
+	float draworder,
+	std::ostream & error_output)
+{
+	Unload(parent);
+
+	assert(!draw.valid());
+	assert(!node.valid());
+
+	TEXTUREINFO texinfo;
+	texinfo.SetName(texturefile);
+	texinfo.SetMipMap(false);
+	texinfo.SetRepeat(false, false);
+	texinfo.SetAllowNonPowerOfTwo(false);
+	texinfo.SetSize(texturesize);
+	std::tr1::shared_ptr<TEXTURE> texture(textures.Get(info)); 
+	if (!texture->Loaded()) return false;
+
+	node = parent.AddNode();
+	SCENENODE & noderef = parent.GetNode(node);
+	draw = noderef.GetDrawlist().twodim.insert(DRAWABLE());
+	DRAWABLE & drawref = GetDrawableFromNode(noderef);
+
+	drawref.SetDiffuseMap(texture);
+	drawref.SetVertArray(&varray);
+	drawref.SetDrawOrder(draworder);
+	drawref.SetCull(false, false);
+	drawref.SetColor(r,g,b,a);
+
+	//std::cout << "Sprite draworder: " << draworder << std::endl;
+
+	return true;
+}
+
+bool SPRITE2D::Load(
+	SCENENODE & parent,
+	std::tr1::shared_ptr<TEXTURE> texture2d,
+	float draworder,
+	std::ostream & error_output)
+{
+	Unload(parent);
+
+	assert(!draw.valid());
+	assert(!node.valid());
+
+	node = parent.AddNode();
+	SCENENODE & noderef = parent.GetNode(node);
+	draw = noderef.GetDrawlist().twodim.insert(DRAWABLE());
+	DRAWABLE & drawref = GetDrawableFromNode(noderef);
+
+	drawref.SetDiffuseMap(texture2d);
+	drawref.SetVertArray(&varray);
+	drawref.SetDrawOrder(draworder);
+	drawref.SetCull(false, false);
+	drawref.SetColor(r,g,b,a);
+	
+	return true;
+}
