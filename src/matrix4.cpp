e6089b72dce22814645d692b33a37a671c2530ef
joevenzon <joevenzon@f68921f7-4efc-0310-90ae-aa24c258f575>
2010-12-17 15:27:13 +0000

    Fixed the matrix unit test.

--- src/matrix4.cpp
+++ src/matrix4.cpp
@@ -19,7 +19,7 @@ QT_TEST(matrix4_test)
 	MATHVECTOR <float, 3> orig = out;
 	mat.TransformVectorOut(out[0], out[1], out[2]);
 	
-	MATHVECTOR <float, 3> comp(out);
+	MATHVECTOR <float, 3> comp(orig);
 	quat.RotateVector(comp);
 	comp = comp + vec;
 	
@@ -27,10 +27,10 @@ QT_TEST(matrix4_test)
 	QT_CHECK_CLOSE(out[1], comp[1], 0.001);
 	QT_CHECK_CLOSE(out[2], comp[2], 0.001);
 	
-	MATHVECTOR <float, 3> in;
-	mat.TransformVectorOut(in[0], in[1], in[2]);
+	MATHVECTOR <float, 3> in(out);
+	mat.TransformVectorIn(in[0], in[1], in[2]);
 	
-	QT_CHECK_CLOSE(out[0], orig[0], 0.001);
-	QT_CHECK_CLOSE(out[1], orig[1], 0.001);
-	QT_CHECK_CLOSE(out[2], orig[2], 0.001);
+	QT_CHECK_CLOSE(in[0], orig[0], 0.001);
+	QT_CHECK_CLOSE(in[1], orig[1], 0.001);
+	QT_CHECK_CLOSE(in[2], orig[2], 0.001);
 }
