34b6777f475d75d38580dcc7d49995cab1d4c2ea
NaN <NaN@f68921f7-4efc-0310-90ae-aa24c258f575>
2010-12-24 01:38:22 +0000

    Move objectloader constructor into header due msvc linker issues.

--- src/objectloader.cpp
+++ src/objectloader.cpp
@@ -31,51 +31,6 @@ static bool GetParam(std::ifstream & f, T & output)
 	return true;
 }
 
-OBJECTLOADER::OBJECTLOADER(
-	SCENENODE & sceneroot,
-	TEXTUREMANAGER & texture_manager,
-	MODELMANAGER & model_manager,
-	std::vector<std::tr1::shared_ptr<MODEL> > & models,
-	std::vector<TRACKOBJECT> & objects,
-	std::ostream & info_output,
-	std::ostream & error_output,
-	const std::vector<TRACKSURFACE> & surfaces,
-	const std::string & trackpath,
-	const std::string & trackdir,
-	const std::string & texsize,
-	const int anisotropy,
-	const bool vertical_tracking_skyboxes,
-	const bool dynamicshadowsenabled,
-	const bool agressivecombine,
-	const bool cull) :
-	sceneroot(sceneroot),
-	texture_manager(texture_manager),
-	model_manager(model_manager),
-	models(models),
-	objects(objects),
-	info_output(info_output),
-	error_output(error_output),
-	surfaces(surfaces),
-	trackpath(trackpath),
-	trackdir(trackdir),
-	texsize(texsize),
-	anisotropy(anisotropy),
-	vertical_tracking_skyboxes(vertical_tracking_skyboxes),
-	dynamicshadowsenabled(dynamicshadowsenabled),
-	agressivecombine(agressivecombine),
-	cull(cull),
-	objectpath(trackpath + "/objects"),
-	objectdir(trackdir +  "/objects"),
-	packload(false),
-	numobjects(0),
-	params_per_object(17),
-	expected_params(17),
-	min_params(14),
-	error(false)
-{
-	// ctor
-}
-
 bool OBJECTLOADER::BeginObjectLoad()
 {
 	CalculateNumObjects();
