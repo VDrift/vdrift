b6669714f294b772c01d5c5fafcc17032899eefa
NaN <NaN@f68921f7-4efc-0310-90ae-aa24c258f575>
2011-02-23 19:46:09 +0000

    trackobject position, rotation

--- include/trackobject.h
+++ include/trackobject.h
@@ -1,6 +1,9 @@
 #ifndef _TRACK_OBJECT_H
 #define _TRACK_OBJECT_H
 
+#include "mathvector.h"
+#include "quaternion.h"
+
 class MODEL;
 class TRACKSURFACE;
 
@@ -8,10 +11,10 @@ struct TRACKOBJECT
 {
 	const MODEL * model;
 	const TRACKSURFACE * surface;
+	MATHVECTOR<float, 3> position;
+	QUATERNION<float> rotation;
 	
-	TRACKOBJECT() : model(0), surface(0) {}
-	
-	TRACKOBJECT(const MODEL * model, const TRACKSURFACE * surface) : model(model), surface(surface) {}
+	TRACKOBJECT() : model(0), surface(0) { }
 };
 
 #endif
