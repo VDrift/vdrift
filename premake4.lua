8f0a144645607a69aa03a8332ee0da85fb2d5a19
Antonio Vázquez Blanco <antoniovazquezblanco@gmail.com>
2011-08-10 16:22:18 +0200

    Finish premake for posix.

--- premake4.lua
+++ premake4.lua
@@ -1,36 +1,38 @@
 solution "VDrift"
 	project "vdrift"
-		kind "ConsoleApp"
+		kind "WindowedApp"
 		language "C++"
 		location "."
 		targetdir "."
-		includedirs {"include", "src"}
-		links {"SDL_image", "SDL_gfx", "vorbisfile", "curl", "archive"}
-		files {"include/**.h", "src/**.cpp"}
-	
+		includedirs {"src"}
+		files {"src/**"}
+		location "build"
+
 	configurations {"Debug", "Release"}
-	
+
 	configuration "Release"
 		defines {"NDEBUG"}
 		flags {"OptimizeSpeed"}
-	
+
 	configuration "Debug"
 		defines {"DEBUG"}
 		flags {"ExtraWarnings", "Symbols"}
-	
+
 	configuration {"windows"}
 		flags {"StaticRuntime"}
 		includedirs {"win32/include", "win32/bullet"}
 		libdirs {"win32/lib"}
 		links {"opengl32", "glu32", "glew32", "SDLmain", "SDL", "wsock32", "ws2_32"}
 		files {"win32/bullet/**.h", "win32/bullet/**.cpp"}
+		links {"SDL_image", "SDL_gfx", "vorbisfile", "curl", "archive"}
 		postbuildcommands {"xcopy /d /y /f win32\lib\*.dll"}
-	
+
 	configuration {"windows", "codeblocks"}
 		links {"mingw32"}
-		
+
 	configuration {"linux"}
+		includedirs {"/usr/local/include/bullet/"}
 		libdirs {"/usr/X11R6/lib"}
-        links {"GL", "GLU", "GLEW"}
-		
+		links {"archive", "curl", "LinearMath", "BulletDynamics", "BulletCollision", "GL", "GLU", "GLEW", "SDL", "vorbisfile", "SDL_image", "SDL_gfx"}
+
 	configuration {"macosx"}
