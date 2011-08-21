03d378bfacb746d5e58e844bc19c852077e3ed5c
Antonio Vázquez Blanco <antoniovazquezblanco@gmail.com>
2011-08-21 13:21:34 +0200

    cfg/xml: Added GPL v3 header, make comments fit conventions and optimize some code.

--- src/cfg/xml.cpp
+++ src/cfg/xml.cpp
@@ -1,15 +1,39 @@
-#include "ptree.h"
+/************************************************************************/
+/*                                                                      */
+/* This file is part of VDrift.                                         */
+/*                                                                      */
+/* VDrift is free software: you can redistribute it and/or modify       */
+/* it under the terms of the GNU General Public License as published by */
+/* the Free Software Foundation, either version 3 of the License, or    */
+/* (at your option) any later version.                                  */
+/*                                                                      */
+/* VDrift is distributed in the hope that it will be useful,            */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of       */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        */
+/* GNU General Public License for more details.                         */
+/*                                                                      */
+/* You should have received a copy of the GNU General Public License    */
+/* along with VDrift.  If not, see <http://www.gnu.org/licenses/>.      */
+/*                                                                      */
+/* This is the main entry point for VDrift.                             */
+/*                                                                      */
+/************************************************************************/
 
 /*
-<!--xml-->
-<key1>value1</key1>
-<key2>
-    <key3>
-        <key4>value4</key4>
-	</key3>
-    <key5>value5</key5>
-</key2>
-*/
+ * XML file format:
+ *
+ * <!-- comment -->
+ * <key1>value1</key1>
+ * <key2>
+ *    <key3>
+ *        <key4>value4</key4>
+ *    </key3>
+ *    <key5>value5</key5>
+ * </key2>
+ *
+ */
+
+#include "ptree.h"
 
 static void read_xml(std::istream & in, PTree & p, std::string key)
 {
@@ -18,23 +42,17 @@ static void read_xml(std::istream & in, PTree & p, std::string key)
 	{
 		std::getline(in, line, '\n');
 		if (line.empty())
-		{
 			continue;
-		}
 
 		size_t begin = line.find_first_not_of(" \t\n<");
 		size_t end = line.length();
 		if (begin >= end || line[begin] == '!')
-		{
 			continue;
-		}
 
 		line = line.substr(begin, end);
 
 		if (line.find(escape) == 0)
-		{
 			break;
-		}
 
 		if (key.length() == 0)
 		{
@@ -62,7 +80,6 @@ static void read_xml(std::istream & in, PTree & p, std::string key)
 static void write_xml(const PTree & p, std::ostream & out, std::string indent)
 {
 	for (PTree::const_iterator i = p.begin(), e = p.end(); i != e; ++i)
-	{
 		if (i->second.size() == 0)
 		{
 			out << indent << "<" << i->first << ">" << i->second.value() << "</" << i->first << ">\n";
@@ -74,7 +91,6 @@ static void write_xml(const PTree & p, std::ostream & out, std::string indent)
 			write_xml(i->second, out, indent+"\t");
 			out << indent << "</" << i->first << ">\n";
 		}
-	}
 }
 
 void read_xml(std::istream & in, PTree & p)
