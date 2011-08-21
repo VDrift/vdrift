6ffa1feaac4b454ccc144bd5b26133579b7a3a20
Antonio Vázquez Blanco <antoniovazquezblanco@gmail.com>
2011-08-21 13:24:22 +0200

    cfg/ini: Added GPL v3 header, make comments fit conventions and optimize some code.

--- src/cfg/ini.cpp
+++ src/cfg/ini.cpp
@@ -1,15 +1,39 @@
-#include "ptree.h"
+/************************************************************************/
+/*                                                                      */
+/* This file is part of VDrift.                                         */
+/*                                                                      */
+/* VDrift is free software: you can redistribute it and/or modify       */
+/* it under the terms of the GNU General Public License as published by */
+/* the Free Software Foundation, either version 3 of the License, or    */
+/* (at your option) any later version.                                  */
+/*                                                                      */
+/* VDrift is distributed in the hope that it will be useful,            */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of       */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        */
+/* GNU General Public License for more details.                         */
+/*                                                                      */
+/* You should have received a copy of the GNU General Public License    */
+/* along with VDrift.  If not, see <http://www.gnu.org/licenses/>.      */
+/*                                                                      */
+/* This is the main entry point for VDrift.                             */
+/*                                                                      */
+/************************************************************************/
 
 /*
-# ini
-key1 = value1
+ * INI file structure:
+ *
+ * # comment
+ * key1 = value1
+ *
+ * [key2]
+ * key5 = value5
+ *
+ * [key2.key3]
+ * key4 = value4
+ *
+ */
 
-[key2]
-key5 = value5
-
-[key2.key3]
-key4 = value4
-*/
+#include "ptree.h"
 
 struct ini
 {
@@ -18,10 +42,9 @@ struct ini
 	const file_open * fopen;
 	PTree cache;
 
-	ini(std::istream & in, PTree & root, const file_open * fopen = 0) :
-		in(in), root(root), fopen(fopen)
+	ini(std::istream & in, PTree & root, const file_open * fopen = 0) : in(in), root(root), fopen(fopen)
 	{
-		// ctor
+		// Constructor.
 	}
 
 	void read()
@@ -36,21 +59,17 @@ struct ini
 		{
 			std::getline(in, line, '\n');
 			if (line.empty())
-			{
 				continue;
-			}
 
 			size_t begin = line.find_first_not_of(" \t[");
 			size_t end = line.find_first_of(";#]\r", begin);
 			if (begin >= end)
-			{
 				continue;
-			}
 
 			size_t next = line.find("=", begin);
 			if (next >= end)
 			{
-				// new node
+				// New node.
 				next = line.find_last_not_of(" \t\r]", end);
 				name = line.substr(begin, next);
 				read(root.set(name, PTree()));
@@ -60,20 +79,18 @@ struct ini
 			size_t next2 = line.find_first_not_of(" \t\r", next+1);
 			next = line.find_last_not_of(" \t", next-1);
 			if (next2 >= end)
-			{
 				continue;
-			}
 
 			name = line.substr(begin, next+1);
 			if (!fopen || line.at(next2) != '&')
 			{
-				// new property
+				// New property.
 				std::string value = line.substr(next2, end-next2);
 				node.set(name, value);
 				continue;
 			}
 
-			// value is a reference
+			// Value is a reference.
 			std::string value = line.substr(next2+1, end-next2-1);
 			const PTree * ref_ptr;
 			if (root.get(value, ref_ptr) || cache.get(value, ref_ptr))
@@ -82,12 +99,11 @@ struct ini
 				continue;
 			}
 
-			// load external reference
+			// Load external reference.
 			PTree ref;
 			read_ini(value, *fopen, ref);
 			cache.set(value, ref);
 			node.set(name, ref);
-			//write_inf(node, std::cerr);
 		}
 	}
 };
@@ -95,22 +111,16 @@ struct ini
 static void write_ini(const PTree & p, std::ostream & out, std::string key_name)
 {
 	for (PTree::const_iterator i = p.begin(), e = p.end(); i != e; ++i)
-	{
 		if (i->second.size() == 0)
-		{
 			out << i->first << " = " << i->second.value() << "\n";
-		}
-	}
 	out << "\n";
 
 	for (PTree::const_iterator i = p.begin(), e = p.end(); i != e; ++i)
-	{
 		if (i->second.size() > 0)
 		{
 			out << "[" << key_name + i->first << "]\n";
 			write_ini(i->second, out, key_name + i->first + ".");
 		}
-	}
 }
 
 void read_ini(std::istream & in, PTree & p)
