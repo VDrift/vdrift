9cdd2a0302ff12b59b6f5e1fb3c277b7ad30cb25
Antonio Vázquez Blanco <antoniovazquezblanco@gmail.com>
2011-08-22 23:26:57 +0200

    src/gl3v/glenums: Move code to cpp file instead of placing it on the header one, add some documentation comments and add GPL v3 header.

--- src/gl3v/glenums.cpp
+++ src/gl3v/glenums.cpp
@@ -19,37 +19,34 @@
 /*                                                                      */
 /************************************************************************/
 
-#ifndef _GLENUMS_H
-#define _GLENUMS_H
+#include "glenums.h"
 
-#ifdef __APPLE__
-#include <GLEW/glew.h>
-#include <OpenGL/gl.h>
-#else
-#include <GL/glew.h>
-#include <GL/gl.h>
-#endif
-#include <string>
-#include <cassert>
-#include "unordered_map.h"
-
-/// Base interface for OpenGL enumerations.
-class GLEnums
+GLEnums::GLEnums()
 {
-public:
-	GLEnums();
+	#define X(x) {enumToStr[x]=#x;strToEnum[#x]=x;}
+	#include "glenums.def"
+	#undef X
+}
 
-	/// Get OpenGL enumeration based on the string.
-	GLenum getEnum(const std::string & value) const
+GLenum GLEnums::getEnum(const std::string & value) const
+{
+	std::tr1::unordered_map <std::string, GLenum>::const_iterator i = strToEnum.find(value);
+	if (i == strToEnum.end())
+	{
+		std::cerr << "Unknown GLenum string: " << value << std::endl;
+		assert(0);
+	}
+	return i->second;
+}
 
-	/// Get the string based on an OpenGL enumeration.
-	const std::string & getEnum(GLenum value) const;
+const std::string & GLEnums::getEnum(GLenum value) const
+{
+	std::tr1::unordered_map <GLenum, std::string>::const_iterator i = enumToStr.find(value);
+	if (i == enumToStr.end())
+	{
+		std::cerr << "Unknown GLenum value: " << (int)value << std::endl;
+		assert(0);
+	}
+	return i->second;
+}
 
-private:
-	std::tr1::unordered_map <std::string, GLenum> strToEnum;
-	std::tr1::unordered_map <GLenum, std::string> enumToStr;
-};
-
-#undef ADD_GL_ENUM
-
-#endif
