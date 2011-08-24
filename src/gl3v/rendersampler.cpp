86af14a0affa486f9ccf6983b24cca05d21bcf0c
Antonio Vázquez Blanco <antoniovazquezblanco@gmail.com>
2011-08-24 16:44:28 +0200

    Rearrange gl3v/rendersampler.

--- src/gl3v/rendersampler.cpp
+++ src/gl3v/rendersampler.cpp
@@ -1,11 +1,48 @@
+/************************************************************************/
+/*                                                                      */
+/* This file is part of VDrift.                                         */
+/*                                                                      */
+/* VDrift is free software: you can redistribute it and/or modify       */
+/* it under the terms of the GNU General Public License as published by */
+/* the Free Software Foundation, either version 3 of the License, or    */
+/* (at your option) any later version.                                  */
+/*                                                                      */
+/* VDrift is distributed in the hope that it will be useful,            */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of       */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        */
+/* GNU General Public License for more details.                         */
+/*                                                                      */
+/* You should have received a copy of the GNU General Public License    */
+/* along with VDrift.  If not, see <http://www.gnu.org/licenses/>.      */
+/*                                                                      */
+/************************************************************************/
+
 #include "rendersampler.h"
+
+RenderSampler::RenderSampler(GLuint newTu, GLuint newHandle) : tu(newTu), handle(newHandle)
+{
+	// Constructor.
+}
 
 void RenderSampler::apply(GLWrapper & gl) const
 {
 	for (std::vector <RenderState>::const_iterator s = state.begin(); s != state.end(); s++)
-	{
 		s->applySampler(gl, handle);
-	}
 
 	gl.BindSampler(tu, handle);
+}
+
+void RenderSampler::addState(const RenderState & newState)
+{
+	state.push_back(newState);
+}
+
+GLuint RenderSampler::getHandle() const
+{
+	return handle;
+}
+
+const std::vector <RenderState> & RenderSampler::getStateVector() const
+{
+	return state;
 }
--- src/gl3v/rendersampler.h
+++ src/gl3v/rendersampler.h
@@ -1,3 +1,22 @@
+/************************************************************************/
+/*                                                                      */
+/* This file is part of VDrift.                                         */
+/*                                                                      */
+/* VDrift is free software: you can redistribute it and/or modify       */
+/* it under the terms of the GNU General Public License as published by */
+/* the Free Software Foundation, either version 3 of the License, or    */
+/* (at your option) any later version.                                  */
+/*                                                                      */
+/* VDrift is distributed in the hope that it will be useful,            */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of       */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        */
+/* GNU General Public License for more details.                         */
+/*                                                                      */
+/* You should have received a copy of the GNU General Public License    */
+/* along with VDrift.  If not, see <http://www.gnu.org/licenses/>.      */
+/*                                                                      */
+/************************************************************************/
+
 #ifndef _RENDERSAMPLER
 #define _RENDERSAMPLER
 
@@ -6,18 +25,17 @@
 
 class RenderSampler
 {
-	public:
-		void apply(GLWrapper & gl) const;
-		void addState(const RenderState & newState) {state.push_back(newState);}
-		GLuint getHandle() const {return handle;}
-		const std::vector <RenderState> & getStateVector() const {return state;} ///< used for debug printing
+public:
+	RenderSampler(GLuint newTu, GLuint newHandle);
+	void apply(GLWrapper & gl) const;
+	void addState(const RenderState & newState);
+	GLuint getHandle() const;
+	const std::vector <RenderState> & getStateVector() const; ///< Used for debug printing.
 
-		RenderSampler(GLuint newTu, GLuint newHandle) : tu(newTu), handle(newHandle) {}
-
-	private:
-		std::vector <RenderState> state;
-		GLuint tu;
-		GLuint handle;
+private:
+	std::vector <RenderState> state;
+	GLuint tu;
+	GLuint handle;
 };
 
 #endif
