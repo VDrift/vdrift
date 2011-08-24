f133b4a1165ab86d10bf3bfe507e831058a4b60b
Antonio Vázquez Blanco <antoniovazquezblanco@gmail.com>
2011-08-25 00:11:56 +0200

    crashdetection: Add GPL header and move code to cpp file. Just needs some documentation.

--- src/crashdetection.cpp
+++ src/crashdetection.cpp
@@ -1,14 +1,28 @@
-#include "crashdetection.h"
+/************************************************************************/
+/*                                                                      */
+/* This file is part of VDrift.                                         */
+/*                                                                      */
+/* VDrift is free software: you can redistribute it and/or modify       */
+/* it under the terms of the GNU General Public License as published by */
+/* the Free Software Foundation, either version 3 of the License, or    */
+/* (at your option) any later version.                                  */
+/*                                                                      */
+/* VDrift is distributed in the hope that it will be useful,            */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of       */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        */
+/* GNU General Public License for more details.                         */
+/*                                                                      */
+/* You should have received a copy of the GNU General Public License    */
+/* along with VDrift.  If not, see <http://www.gnu.org/licenses/>.      */
+/*                                                                      */
+/************************************************************************/
 
 #include <cassert>
+#include "crashdetection.h"
 
-CRASHDETECTION::CRASHDETECTION()
-: lastvel(0),
-  curmaxdecel(0),
-  maxdecel(0),
-  deceltrigger(200)
+CRASHDETECTION::CRASHDETECTION() : lastvel(0), curmaxdecel(0), maxdecel(0), deceltrigger(200)
 {
-
+    // Constructor.
 }
 
 void CRASHDETECTION::Update(float vel, float dt)
@@ -17,20 +31,14 @@ void CRASHDETECTION::Update(float vel, float dt)
 
 	float decel = (lastvel - vel)/dt;
 
-	//std::cout << "Decel: " << decel << std::endl;
-
 	if (decel > deceltrigger && curmaxdecel == 0)
-	{
-		//idle, start capturing decel
+		// Idle, start capturing decel.
 		curmaxdecel = decel;
-	}
 	else if (curmaxdecel > 0)
 	{
-		//currently capturing, check for max
+		// Currently capturing, check for max.
 		if (decel > curmaxdecel)
-		{
 			curmaxdecel = decel;
-		}
 		else
 		{
 			maxdecel = curmaxdecel;
@@ -40,4 +48,9 @@ void CRASHDETECTION::Update(float vel, float dt)
 	}
 
 	lastvel = vel;
+}
+
+float CRASHDETECTION::GetMaxDecel() const
+{
+    return maxdecel;
 }
