4ee6b7fd99876c936c16918f566707701e83e55c
Antonio Vázquez Blanco <antoniovazquezblanco@gmail.com>
2011-09-01 18:15:46 +0200

    downloadable: Rearrange code, add GPL v3 header and make comments follow conventions.

--- src/downloadable.h
+++ src/downloadable.h
@@ -1,3 +1,22 @@
+/************************************************************************/
+/*                                                                      */
+/* This file is part of VDrift.                                         */
+/*                                                                      */
+/* VDrift is free software: you can redistribute it and/or modify       */
+/* it under the terms of the GNU General Public License as published by */
+/* the Free Software Foundation, either version 3 of the License, or    */
+/* (at your option) any later version.                                  */
+/*                                                                      */
+/* VDrift is distributed in the hope that it will be useful,            */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of       */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        */
+/* GNU General Public License for more details.                         */
+/*                                                                      */
+/* You should have received a copy of the GNU General Public License    */
+/* along with VDrift.  If not, see <http://www.gnu.org/licenses/>.      */
+/*                                                                      */
+/************************************************************************/
+
 #ifndef _DOWNLOADABLE_H
 #define _DOWNLOADABLE_H
 
@@ -5,25 +24,24 @@
 #include <map>
 #include <vector>
 
-/// A dictionary of downloadable assets
+/// A dictionary of downloadable assets.
 class DOWNLOADABLEMANAGER
 {
 public:
-	/// initialize using the provided filename as the record-keeping method
-	/// the file doesn't need to exist, but if it does, it will be parsed
-	/// to get local_version info
+	/// Initialize using the provided filename as the record-keeping method.
+	/// The file doesn't need to exist, but if it does, it will be parsed to get local_version info.
 	void Initialize(const std::string & newfilename);
 
-	/// given a map of available downloadables and their remote version,
-	/// return a list of downloadable names that we want to download
+	/// Given a map of available downloadables and their remote version, return a list of downloadable names that we want to download.
 	std::vector <std::string> GetUpdatables(const std::map <std::string, int> & remote_downloadables) const;
 
-	/// inform us that we have just installed a new downloadable
+	/// Inform us that we have just installed a new downloadable.
 	void SetDownloadable(const std::string & name, int new_version);
 
 private:
 	std::string filename;
-	std::map <std::string, int> downloadables; ///< mapping between downloadable name and local version
+    /// Mapping between downloadable name and local version.
+	std::map <std::string, int> downloadables;
 
 	void Load();
 	void Save() const;
