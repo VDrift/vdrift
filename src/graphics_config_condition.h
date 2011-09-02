9355fb695b7063d5733f2e3d58e8b9c8fed78822
Antonio Vázquez Blanco <antoniovazquezblanco@gmail.com>
2011-09-02 11:39:12 +0200

    graphics_config_condition: Add header, optimize code and delete unused imports.

--- src/graphics_config_condition.h
+++ src/graphics_config_condition.h
@@ -1,14 +1,30 @@
+/************************************************************************/
+/*                                                                      */
+/* This file is part of VDrift.                                         */
+/*                                                                      */
+/* VDrift is free software: you can redistribute it and/or modify       */
+/* it under the terms of the GNU General Public License as published by */
+/* the Free Software Foundation, either version 3 of the License, or    */
+/* (at your option) any later version.                                  */
+/*                                                                      */
+/* VDrift is distributed in the hope that it will be useful,            */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of       */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        */
+/* GNU General Public License for more details.                         */
+/*                                                                      */
+/* You should have received a copy of the GNU General Public License    */
+/* along with VDrift.  If not, see <http://www.gnu.org/licenses/>.      */
+/*                                                                      */
+/************************************************************************/
+
 #ifndef _GRAPHICS_CONFIG_CONDITION_H
 #define _GRAPHICS_CONFIG_CONDITION_H
 
 #include <string>
-#include <vector>
-#include <map>
-#include <fstream>
-#include <iostream>
 #include <set>
 
-/// a space-delimited list of string conditions. putting ! before the condition negates it.
+/// A space-delimited list of string conditions.
+/// Putting "!" before the condition negates it.
 class GRAPHICS_CONFIG_CONDITION
 {
 public:
