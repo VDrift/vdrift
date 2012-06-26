79f4e161867f0b7e6844f879b24a49f31a413950
Antonio Vázquez Blanco <antoniovazquezblanco@gmail.com>
2012-06-26 20:00:35 +0200

    src/ai/ai: Make code follow coding style.

--- src/ai/ai.h
+++ src/ai/ai.h
@@ -21,30 +21,30 @@
 #define _AI_H
 
 #include "car.h"
-
+#include "ai_car.h"
 #include <string>
 #include <vector>
 #include <map>
 
-class AI_Car;
 class AI_Factory;
 
 /// Manages all AI cars.
 class AI
 {
 private:
-	std::vector <float> empty_input;
 	std::vector <AI_Car*> AI_Cars;
 	std::map <std::string, AI_Factory*> AI_Factories;
+	std::vector <float> empty_input;
+
 public:
 	AI();
 	~AI();
 
-	void add_car(CAR * car, float difficulty, std::string type = default_ai_type );
+	void add_car(CAR * car, float difficulty, std::string type = default_ai_type);
 	void remove_car(CAR * car);
 	void clear_cars();
 	void update(float dt, const std::list <CAR> & othercars);
-	const std::vector <float>& GetInputs(CAR * car) const; ///< returns an empty vector if the car isn't AI-controlled
+	const std::vector <float>& GetInputs(CAR * car) const; ///< Returns an empty vector if the car isn't AI-controlled.
 
 	void AddAIFactory(const std::string& type_name, AI_Factory* factory);
 	std::vector<std::string> ListFactoryTypes();
