f8e7e1c2fed30c7dc923573ca4d29fbcdd662436
logzero <core13@gmx.net>
2012-09-03 17:34:17 +0200

    Fix upper range limit in Tire::getSigmaHatAlphaHat.

--- src/physics/tire.cpp
+++ src/physics/tire.cpp
@@ -62,9 +62,9 @@ void Tire::getSigmaHatAlphaHat(btScalar load, btScalar & sh, btScalar & ah) cons
 
 	int tablesize = sigma_hat.size();
 	btScalar rload = load / max_load * tablesize - 1;
-	btClamp(rload, btScalar(0), btScalar(tablesize - 1));
+	btClamp(rload, btScalar(0), btScalar(tablesize - 1 - 1E-9));
 	int lbound = int(rload);
-	btAssert(lbound < tablesize);
+	btAssert(lbound < tablesize - 2);
 
 	btScalar blend = rload - lbound;
 	sh = sigma_hat[lbound] * (1.0 - blend) + sigma_hat[lbound + 1] * blend;
