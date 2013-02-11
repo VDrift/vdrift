8150d44747212b2333577d3e06a0b8f772873fa7
logzero <core13@gmx.net>
2013-02-11 20:44:04 +0100

    GRAPHICS: Fix varray initializer list compile error.

--- src/graphics/vertexarray.h
+++ src/graphics/vertexarray.h
@@ -147,7 +147,9 @@ public:
 	{
 		VERTEXDATA v[3];
 
-		FACE(VERTEXDATA v1, VERTEXDATA v2, VERTEXDATA v3) : v((VERTEXDATA[3]){v1, v2, v3}) {}
+		FACE(VERTEXDATA v1, VERTEXDATA v2, VERTEXDATA v3) {
+			v[0] = v1; v[1] = v2; v[3] = v3;
+		}
 
 		FACE() {}
 	};
