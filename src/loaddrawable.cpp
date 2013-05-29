dd74474f0a14bb451cda1763a1ef6b2268163148
logzero <core13@gmx.net>
2013-05-29 17:09:20 +0200

    GAME: Third drawable texture is a normal map.

--- src/loaddrawable.cpp
+++ src/loaddrawable.cpp
@@ -84,6 +84,7 @@ bool LoadDrawable::operator()(
 	}
 	if (texname.size() > 2)
 	{
+		texinfo.normalmap = true;
 		content.load(tex, path, texname[2], texinfo);
 		drawable.SetMiscMap2(tex);
 	}
--- src/trackloader.cpp
+++ src/trackloader.cpp
@@ -452,6 +452,7 @@ TRACK::LOADER::body_iterator TRACK::LOADER::LoadBody(const PTree & cfg)
 	}
 	if (texture_names[2].length() > 0)
 	{
+		texinfo.normalmap = true;
 		content.load(miscmap2, objectdir, texture_names[2], texinfo);
 	}
 
@@ -702,6 +703,7 @@ bool TRACK::LOADER::AddObject(const OBJECT & object)
 
 	std::tr1::shared_ptr<TEXTURE> miscmap2_texture;
 	{
+		texinfo.normalmap = true;
 		std::string texture_name = object.texture.substr(0, std::max(0, (int)object.texture.length()-4)) + "-misc2.png";
 		std::string filepath = objectpath + "/" + texture_name;
 		if (std::ifstream(filepath.c_str()))
