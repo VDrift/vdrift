8401fbdb3d23f5e8f7bbc1255fb55e29f6771d59
logzero <core13@gmx.net>
2013-08-12 19:20:27 +0200

    GRAPHICS: Enable gl error checks on win32.

--- src/graphics/glutil.cpp
+++ src/graphics/glutil.cpp
@@ -19,24 +19,17 @@
 
 #include "glutil.h"
 #include "glew.h"
-#include <cassert>
 
 bool GLUTIL::CheckForOpenGLErrors(
 	const std::string & activity_description,
 	std::ostream & error_output)
 {
-#if defined(WIN32) || defined(_WIN32) || defined (__WIN32) || defined(__WIN32__) \
-	|| defined (_WIN64) || defined(__CYGWIN__) || defined(__MINGW32__)
-	//disable error checking because intel has many driver chipsets out with non-compliant OpenGL drivers
-#else
-	GLenum gl_error = glGetError();
-	if (gl_error != GL_NO_ERROR)
+	GLenum error = glGetError();
+	if (error != GL_NO_ERROR)
 	{
-		const GLubyte *err_string = gluErrorString(gl_error);
+		const GLubyte *err_string = gluErrorString(error);
 		error_output << "OpenGL error \"" << err_string << "\" during: " << activity_description << std::endl;
-		//assert (gl_error == GL_NO_ERROR);
 		return true;
 	}
-#endif
 	return false;
 }
