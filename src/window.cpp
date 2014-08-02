11b0990b1a05308873e55c9dd74ede2e8bbab53f
logzero <core13@gmx.net>
2014-08-02 12:52:47 +0200

    GRAPHICS: Enable GL 3.3 Core Profile. Tested gl2 and gl3 renderers on Linux/Mesa and Win7/Catalyst.

--- src/window.cpp
+++ src/window.cpp
@@ -187,11 +187,9 @@ void Window::ChangeDisplay(
 	std::ostream & info_output,
 	std::ostream & error_output)
 {
-#ifdef GL3CORE
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
-#endif
 	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, depth_bpp);
 
